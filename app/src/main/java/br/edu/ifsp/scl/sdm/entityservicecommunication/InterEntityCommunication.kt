5bf59fd4ada18f9f04943da753ec2bbb0fcdb614
Victor Cardoso <vhcardoso30@gmail.com>
2024-03-20 20:47:08 -0300

    Aula 1

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/InterEntityCommunication.kt
@@ -0,0 +1,8 @@
+package br.edu.ifsp.scl.sdm.entityservicecommunication
+
+import androidx.lifecycle.MutableLiveData
+
+object InterEntityCommunication {
+
+    val valueLiveData: MutableLiveData<Int> = MutableLiveData()
+}
